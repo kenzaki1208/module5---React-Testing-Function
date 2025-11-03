0a9826cce93acb9003c0ef911637af1ac4b92a3c
kenzaki1208 <duytrinh25072001@gmail.com>
2025-11-03 15:18:28 +0700

    update project

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ const App = () => {
     const [counter, setCounter] = useState(0);
 
     return (
-        <div>
-            <h1>This is counter app</h1>
-            <div data-testid="counter-value">Count: {counter}</div>
-            <button onClick={() => setCounter(counter + 1)}>Increment</button>
-            <button onClick={() => setCounter(counter - 1)}>Decrement</button>
-        </div>
+        <>
+            <div>
+                <h1>This is counter app</h1>
+                <div data-testid="counter-value">Count: {counter}</div>
+                <button onClick={() => setCounter(counter + 1)}>Increment</button>
+                <button onClick={() => setCounter(counter - 1)}>Decrement</button>
+            </div>
+
+            <div>&copy; CodeGym 2025</div>
+        </>
     );
 };
 
