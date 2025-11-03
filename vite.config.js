e98447b2d3312b9175b32f59ea83ada8e3c81346
kenzaki1208 <duytrinh25072001@gmail.com>
2025-11-03 15:26:05 +0700

    update project

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/REACT-TEST-FUNCTIONS',
+  base: '/module5---React-Testing-Function/',
   test: {
     globals: true,
     environment: "jsdom",
